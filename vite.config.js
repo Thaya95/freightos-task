5078890b7f4c27cdfac0c2be8f8a4f4278d07458
Thaya-ss <thaya@squareshift.co>
2026-06-04 12:39:26 +0530

    update-config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    allowedHosts: [
+      '3bf1-2409-40f4-410e-635-79de-a0d8-fa8b-c34d.ngrok-free.app',
+      'majestic-dango-b2a55a.netlify.app'
+    ]
+  }
 })
